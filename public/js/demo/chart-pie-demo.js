2674f22426fd5e9c9fb437c15c71a87553328e51
Erkan Pulat <erkancsrt@gmail.com>
2021-07-17 00:09:36 +0300

    Admin ana sayfasına dashboard yapıldı. Kullanıcılara ait istatistikler cart nesnelerine yazdırıldı. Pie chart nesneleri ile de gösterimler yapıldı.  Verilerin veri tabanından çekilmesi için controller içine gerekli metodlar yazıldı. Admin ana sayfasındaki sidebar ve topbar partials olarak ayrıldı.

--- public/js/demo/chart-pie-demo.js
+++ public/js/demo/chart-pie-demo.js
@@ -1,26 +1,47 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
+(Chart.defaults.global.defaultFontFamily = "Nunito"),
+  '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = "#858796";
 
-// Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
+// dataInfo
+let thisMthNumOfUsers = document
+  .getElementById("thisMthNumOfUsers")
+  .textContent.trim();
+let totalNumOfUsers = document
+  .getElementById("totalNumOfUsers")
+  .textContent.trim();
+let totalNumOfAdmins = document
+  .getElementById("totalNumOfAdmins")
+  .textContent.trim();
+let emailVerNumOfUsers = document
+  .getElementById("emailVerNumOfUsers")
+  .textContent.trim();
+
+// Pie Chart User
+var ctx = document.getElementById("usersPieChart");
+var usersPieChart = new Chart(ctx, {
+  type: "doughnut",
   data: {
-    labels: ["Direct", "Referral", "Social"],
-    datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
+    labels: [
+      "Kullanıcı Sayısı (Bu Ay)",
+      "Kullanıcı Sayısı (Toplam)",
+      "Admin Sayısı",
+    ],
+    datasets: [
+      {
+        data: [thisMthNumOfUsers, totalNumOfUsers, totalNumOfAdmins],
+        backgroundColor: ["#4e73df", "#1cc88a", "#F6C23E"],
+        hoverBackgroundColor: ["#2e59d9", "#17a673", "#F6B13E"],
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+      },
+    ],
   },
   options: {
     maintainAspectRatio: false,
     tooltips: {
       backgroundColor: "rgb(255,255,255)",
       bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
+      borderColor: "#dddfeb",
       borderWidth: 1,
       xPadding: 15,
       yPadding: 15,
@@ -28,8 +49,44 @@ var myPieChart = new Chart(ctx, {
       caretPadding: 10,
     },
     legend: {
-      display: false
+      display: false,
     },
     cutoutPercentage: 80,
   },
 });
+
+// Pie Chart Email
+var ctx = document.getElementById("emailPieChart");
+var emailPieChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["E-postası Onaylı Kullanıcı", "E-postası Onaylanmamış Kullanıcı"],
+    datasets: [
+      {
+        data: [emailVerNumOfUsers, totalNumOfUsers - emailVerNumOfUsers],
+        backgroundColor: ["#1cc88a", "#4e73df"],
+        hoverBackgroundColor: ["#17a673", "#2e59d9"],
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: "#dddfeb",
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false,
+    },
+    cutoutPercentage: 80,
+  },
+});
+
+// console.log(usersPieChart.data.datasets[0].data);
--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -1,6 +1,47 @@
+const User = require("../models/User");
+
 // user home page
-const getHomePage = (req, res, next) => {
-  res.render("pages/admin/home_page", { layout: "admin/home_layout" });
+const getHomePage = async (req, res, next) => {
+  try {
+    // total number of users
+    const totalNumOfUsers = await User.countDocuments({ isAdmin: false });
+
+    // number of users this month
+    const thisMthNumOfUsers = await User.countDocuments({
+      isAdmin: false,
+      createdAt: {
+        $gt: new Date(
+          // this month / 01 / this year 00:00:00
+          `${new Date().getMonth() + 1}/01/${new Date().getFullYear()}`
+        ),
+      },
+    });
+
+    // Number of email verified users
+    const emailVerNumOfUsers = await User.countDocuments({
+      isAdmin: false,
+      emailActive: true,
+    });
+
+    // total number of admin
+    const totalNumOfAdmins = await User.countDocuments({
+      isAdmin: true,
+    });
+
+    res.render("pages/admin/home_page", {
+      layout: "admin/home_layout",
+      totalNumOfUsers,
+      thisMthNumOfUsers,
+      emailVerNumOfUsers,
+      emailVerPercUsers: parseInt(
+        (emailVerNumOfUsers / totalNumOfUsers) * 100,
+        10
+      ),
+      totalNumOfAdmins,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
